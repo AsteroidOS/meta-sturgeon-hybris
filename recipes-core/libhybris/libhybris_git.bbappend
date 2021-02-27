AsteroidOS/meta-smelt-hybris

libhybris: Bump SRCREV to latest.

recipes-core/libhybris/libhybris_git.bbappend
@@ -1,0 +1,1 @@
+SRCREV_smelt = "8883e9aca7de870a4a93b31e82cfc600c51cef5d"

AsteroidOS/meta-sturgeon-hybris

recipes-core/libhybris/libhybris_git.bbappend
@@ -1,0 +1,1 @@
+SRCREV_sturgeon = "8883e9aca7de870a4a93b31e82cfc600c51cef5d"